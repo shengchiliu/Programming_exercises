5c948fb8dccd369c1cb3b9e9682768f0bd6356e2
shengchiliu <scl57@cam.ac.uk>
2018-03-06 22:43:46 +0000

    added file

---
+++ C_exercises/return_array_from_function_2.c
@@ -0,0 +1,23 @@
+// Pass the returned array as parameter
+
+#include <stdio.h>
+
+void fibonacciSeries(int arr[], int size) {
+	arr[0] = 0; arr[1] = 1;
+	for(int i=2; i<size; i++) {
+		arr[i] = arr[0] + arr[1];
+		arr[0] = arr[1];
+		arr[1] = arr[i];
+	}
+	arr[0] = 0; arr[1] = 1;
+}
+
+int main() {
+	int Num=15, arr[Num];
+	fibonacciSeries(arr, Num);
+	for (int i=0; i<Num; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+ 	return 0;
+}
