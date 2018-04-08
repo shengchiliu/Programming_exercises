1faf6ee7af7388039ccf5730565413a862240446
shengchiliu <scl57@cam.ac.uk>
2018-04-08 23:04:47 +0100

    modified

--- PHP_exercises/openfile.php
+++ PHP_exercises/openfile.php
@@ -1,8 +1,8 @@
 <?php
 $myfile = fopen("text.txt", "a") or die("Unable to open file!");
-$txt = "Vincent Liu\n";
-fwrite($myfile, $txt);
-$txt = "Claire Lin\n";
+$name1 = "Vincent Liu";
+$name2 = "Claire Lin";
+$txt = "\n" . $name1 . "\n" . $name2 . "\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
